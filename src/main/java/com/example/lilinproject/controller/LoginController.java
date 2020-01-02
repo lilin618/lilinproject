1e7aaeb6520d9ddade543fc7ea6d222099a324ef
lilin618 <ddlin@139.com>
2020-01-02 21:04:09 +0800

    添加登录模块，完善注册功能 2020-01-02 21点03分

--- src/main/java/com/example/lilinproject/controller/LoginController.java
+++ src/main/java/com/example/lilinproject/controller/LoginController.java
@@ -1,21 +1,22 @@
 package com.example.lilinproject.controller;
 
 import com.example.lilinproject.pojo.User;
-import com.example.lilinproject.resposity.UserResposity;
 import com.example.lilinproject.service.UserService;
 import com.example.lilinproject.utils.MD5Util;
-import com.sun.org.apache.xpath.internal.operations.Mod;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
 
+@Slf4j
 @Controller
 public class LoginController {
 
@@ -26,15 +27,42 @@ public class LoginController {
     public String registerPage(){
         return "register";
     }
-
     @RequestMapping("/login")
-    @ResponseBody
-    public User login(HttpServletRequest request){
-        String username = request.getParameter("username");
-        return userService.findUserByUserName(username);
+    public String login(){
+        return "login";
     }
 
-    @RequestMapping("/UserRegister")
+    @PostMapping("/UserLogin")
+    @ResponseBody
+    public String UserLogin(HttpServletRequest request, HttpSession session){
+        String username = request.getParameter("username");
+        String pwd = request.getParameter("pwd");
+        String msg = "error";
+        try{
+            User user = userService.findUserByUserName(username);
+            if(user == null){
+                msg = "NotFoundUser";
+            }else{
+                if(!user.getIsVaild().equals("Y")){
+                    msg = "NotIsVaild";
+                }else{
+                    boolean loginFlag = new MD5Util().verify(pwd,user.getPassword());
+                    if(loginFlag){
+                        msg = "success";
+                        session.setAttribute("userInfo",user);
+                        session.setMaxInactiveInterval(1800);
+                    }else {
+                        msg = "fail";
+                    }
+                }
+            }
+        }catch (Exception e){
+            log.error("登录异常，异常信息："+e);
+        }
+            return msg;
+    }
+
+    @PostMapping("/UserRegister")
     @ResponseBody
     public String UserRegister(HttpServletRequest request){
         MD5Util md5Util = new MD5Util();
@@ -53,5 +81,18 @@ public class LoginController {
         user.setIsVaild("Y");
         user.setSalt(params.get("salt"));
         return userService.addUser(user);
+    }
+
+    //检测用户名是否可用
+    @PostMapping("/verifyUserName")
+    @ResponseBody
+    public String verifyUserName(HttpServletRequest request){
+        String username = request.getParameter("username");
+        User user = userService.findUserByUserName(username);
+        String result = "fail";
+        if(user != null){
+            result = "ok";
+        }
+        return result;
     }
 }
